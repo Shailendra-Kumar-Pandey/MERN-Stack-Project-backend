72f58256ca5d0f9fabd0c7f34d2118d8f5c3220b
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-11-28 19:25:44 +0530

    add name

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,10 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.get('/', (req, res) => {
-  res.send('Hello Jeetu-Bhai!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello Jeetu-Bhai!')
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`iNotebook backend listening on port http://localhost:${port}`)
 })
