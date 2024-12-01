e455491a923ff1bdef73f2879d592992d227428b
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-02 00:13:39 +0530

    add success method

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -66,6 +66,7 @@ router.post(
     body("password", "Passwod cannot to blank").exists(),
   ],
   async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -75,15 +76,17 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
+        success = false;
         return res
           .status(400)
-          .json({ errors: "please try to login with correct credentials" });
+          .json({ success, errors: "please try to login with correct credentials" });
       }
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
+        success = false;
         return res
           .status(400)
-          .json({ errors: "please try to login with correct credentials" });
+          .json({success, errors: "please try to login with correct credentials" });
       }
       const data = {
         user: {
@@ -91,7 +94,8 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({ authtoken });
+      success = true;
+      res.json({success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
