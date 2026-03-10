be1c07d37f228a00298d7ee69e84c4e196b4a06d
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-10 23:39:48 +0530

    code add

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI = "mongodb://localhost:27017/inotebook";
 // catch(err => console.log(err));
 
  async function connectToMongo(){
     try{
-    await mongoose.connect(mongoURI, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000,
-      });
+    await mongoose.connect(mongoURI);
     console.log("connect to mongo successfully"); 
 }catch(err){
     console.log(err)
@@ -19,14 +15,3 @@ const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&app
 }
 
 module.exports = connectToMongo;
-
-// const mongoose = require('mongoose');
-
-// main().catch(err => console.log(err));
-
-// async function main() {
-//   await mongoose.connect('mongodb://127.0.0.1:27017/test');
-//   console.log("connect to mongo successfully");
-//   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-// }
-// module.exports = main;
--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,7 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Jeetu-Bhai!')
-// })
+
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port http://localhost:${port}`)
